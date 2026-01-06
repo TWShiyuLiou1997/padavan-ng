f24f2ba96b35be588d121f6be807f837b7872304
TWShiyuLiou1997 <34940003+TWShiyuLiou1997@users.noreply.github.com>
2026-01-06 23:33:40 +0800

    Update hwnat_api.c

--- trunk/user/utils/hw_nat/hwnat_api.c
+++ trunk/user/utils/hw_nat/hwnat_api.c
@@ -111,133 +111,112 @@ int HwNatGetAllEntries(unsigned int entry_state)
     int i, result;
     struct hwnat_args *args;
 
+    /* 1. 記憶體分配 (保留原版邏輯) */
     /* Max table size is 16K */
     args = malloc(sizeof(struct hwnat_args)+sizeof(struct hwnat_tuple)*1024*16);
     if (!args) {
-	printf("Allocate memory for hwnat_args and hwnat_tuple failed!\n");
-	return HWNAT_FAIL;
+        printf("Allocate memory for hwnat_args and hwnat_tuple failed!\n");
+        return HWNAT_FAIL;
     }
 
     args->entry_state = entry_state;
 
+    /* 2. 呼叫底層驅動 (保留原版邏輯，這行絕對不能改！) */
     if (HwNatIoCtlArgs(HW_NAT_GET_ALL_ENTRIES, args) != HWNAT_SUCCESS) {
-	free(args);
-	return HWNAT_FAIL;
+        free(args);
+        return HWNAT_FAIL;
     }
 
-    printf("Total Entry Count = %d\n",args->num_of_entries);
+    printf("Total Entry Count = %d\n", args->num_of_entries);
 
-    for(i=0;i<args->num_of_entries;i++) {
-	if(args->entries[i].pkt_type==0) { //IPV4_NAPT
-	    printf("IPv4_NAPT=%d : %u.%u.%u.%u:%d->%u.%u.%u.%u:%d => %u.%u.%u.%u:%d->%u.%u.%u.%u:%d\n", \
-			args->entries[i].hash_index, \
-			NIPQUAD(args->entries[i].ing_sipv4), \
-			args->entries[i].ing_sp, \
-			NIPQUAD(args->entries[i].ing_dipv4), \
-			args->entries[i].ing_dp, \
-			NIPQUAD(args->entries[i].eg_sipv4), \
-			args->entries[i].eg_sp, \
-			NIPQUAD(args->entries[i].eg_dipv4), \
-			args->entries[i].eg_dp);
-	} else if(args->entries[i].pkt_type==1) { //IPV4_NAT
-	    printf("IPv4_NAT=%d : %u.%u.%u.%u->%u.%u.%u.%u => %u.%u.%u.%u->%u.%u.%u.%u\n", \
-			args->entries[i].hash_index, \
-			NIPQUAD(args->entries[i].ing_sipv4), \
-			NIPQUAD(args->entries[i].ing_dipv4), \
-			NIPQUAD(args->entries[i].eg_sipv4), \
-			NIPQUAD(args->entries[i].eg_dipv4)); 
-	} else if(args->entries[i].pkt_type==2) { //IPV6_ROUTING
-	    printf("IPv6_1T=%d /DIP: %x:%x:%x:%x:%x:%x:%x:%x\n", \
-			args->entries[i].hash_index, \
-			NIPHALF(args->entries[i].ing_dipv6_0), \
-			NIPHALF(args->entries[i].ing_dipv6_1), \
-			NIPHALF(args->entries[i].ing_dipv6_2), \
-			NIPHALF(args->entries[i].ing_dipv6_3));
-	} else if(args->entries[i].pkt_type==3) { //IPV4_DSLITE
-	    printf("DS-Lite=%d : %u.%u.%u.%u:%d->%u.%u.%u.%u:%d (%x:%x:%x:%x:%x:%x:%x:%x -> %x:%x:%x:%x:%x:%x:%x:%x)\n", \
-			args->entries[i].hash_index, \
-			NIPQUAD(args->entries[i].ing_sipv4),  \
-			args->entries[i].ing_sp,     \
-			NIPQUAD(args->entries[i].ing_dipv4),  \
-			args->entries[i].ing_dp, \
-			NIPHALF(args->entries[i].eg_sipv6_0), \
-			NIPHALF(args->entries[i].eg_sipv6_1), \
-			NIPHALF(args->entries[i].eg_sipv6_2), \
-			NIPHALF(args->entries[i].eg_sipv6_3), \
-			NIPHALF(args->entries[i].eg_dipv6_0), \
-			NIPHALF(args->entries[i].eg_dipv6_1), \
-			NIPHALF(args->entries[i].eg_dipv6_2), \
-			NIPHALF(args->entries[i].eg_dipv6_3));
-	} else if(args->entries[i].pkt_type==4) { //IPV6_3T_ROUTE
-	    printf("IPv6_3T=%d SIP: %x:%x:%x:%x:%x:%x:%x:%x DIP: %x:%x:%x:%x:%x:%x:%x:%x\n", \
-			args->entries[i].hash_index, \
-			NIPHALF(args->entries[i].ing_sipv6_0), \
-			NIPHALF(args->entries[i].ing_sipv6_1), \
-			NIPHALF(args->entries[i].ing_sipv6_2), \
-			NIPHALF(args->entries[i].ing_sipv6_3), \
-			NIPHALF(args->entries[i].ing_dipv6_0), \
-			NIPHALF(args->entries[i].ing_dipv6_1), \
-			NIPHALF(args->entries[i].ing_dipv6_2), \
-			NIPHALF(args->entries[i].ing_dipv6_3));
-	} else if(args->entries[i].pkt_type==5) { //IPV6_5T_ROUTE
-	    if(args->entries[i].ipv6_flowlabel==1) {
-		printf("IPv6_5T=%d SIP: %x:%x:%x:%x:%x:%x:%x:%x DIP: %x:%x:%x:%x:%x:%x:%x:%x (Flow Label=%x)\n", \
-			args->entries[i].hash_index, \
-			NIPHALF(args->entries[i].ing_sipv6_0), \
-			NIPHALF(args->entries[i].ing_sipv6_1), \
-			NIPHALF(args->entries[i].ing_sipv6_2), \
-			NIPHALF(args->entries[i].ing_sipv6_3), \
-			NIPHALF(args->entries[i].ing_dipv6_0), \
-			NIPHALF(args->entries[i].ing_dipv6_1), \
-			NIPHALF(args->entries[i].ing_dipv6_2), \
-			NIPHALF(args->entries[i].ing_dipv6_3), \
-			((args->entries[i].ing_sp << 16) | (args->entries[i].ing_dp))&0xFFFFF);
-	    }else {
-		printf("IPv6_5T=%d SIP: %x:%x:%x:%x:%x:%x:%x:%x (SP:%d) DIP: %x:%x:%x:%x:%x:%x:%x:%x (DP=%d)\n", \
-			args->entries[i].hash_index, \
-			NIPHALF(args->entries[i].ing_sipv6_0), \
-			NIPHALF(args->entries[i].ing_sipv6_1), \
-			NIPHALF(args->entries[i].ing_sipv6_2), \
-			NIPHALF(args->entries[i].ing_sipv6_3), \
-			args->entries[i].ing_sp, \
-			NIPHALF(args->entries[i].ing_dipv6_0), \
-			NIPHALF(args->entries[i].ing_dipv6_1), \
-			NIPHALF(args->entries[i].ing_dipv6_2), \
-			NIPHALF(args->entries[i].ing_dipv6_3), \
-			args->entries[i].ing_dp);
-	    }
-	} else if(args->entries[i].pkt_type==7) { //IPV6_6RD
-	    if(args->entries[i].ipv6_flowlabel==1) {
-		printf("6RD=%d %x:%x:%x:%x:%x:%x:%x:%x->%x:%x:%x:%x:%x:%x:%x:%x [Flow Label=%x]\n", \
-			args->entries[i].hash_index, \
-			NIPHALF(args->entries[i].ing_sipv6_0), \
-			NIPHALF(args->entries[i].ing_sipv6_1), \
-			NIPHALF(args->entries[i].ing_sipv6_2), \
-			NIPHALF(args->entries[i].ing_sipv6_3), \
-			NIPHALF(args->entries[i].ing_dipv6_0), \
-			NIPHALF(args->entries[i].ing_dipv6_1), \
-			NIPHALF(args->entries[i].ing_dipv6_2), \
-			NIPHALF(args->entries[i].ing_dipv6_3), \
-			((args->entries[i].ing_sp << 16) | (args->entries[i].ing_dp))&0xFFFFF);
-			printf("(%u.%u.%u.%u->%u.%u.%u.%u)\n", NIPQUAD(args->entries[i].eg_sipv4), NIPQUAD(args->entries[i].eg_dipv4));
-	    }else {
-		printf("6RD=%d /SIP: %x:%x:%x:%x:%x:%x:%x:%x [SP:%d] /DIP: %x:%x:%x:%x:%x:%x:%x:%x [DP=%d]", \
-			args->entries[i].hash_index, \
-			NIPHALF(args->entries[i].ing_sipv6_0), \
-			NIPHALF(args->entries[i].ing_sipv6_1), \
-			NIPHALF(args->entries[i].ing_sipv6_2), \
-			NIPHALF(args->entries[i].ing_sipv6_3), \
-			args->entries[i].ing_sp, \
-			NIPHALF(args->entries[i].ing_dipv6_0), \
-			NIPHALF(args->entries[i].ing_dipv6_1), \
-			NIPHALF(args->entries[i].ing_dipv6_2), \
-			NIPHALF(args->entries[i].ing_dipv6_3), \
-			args->entries[i].ing_dp); 
-		printf("(%u.%u.%u.%u->%u.%u.%u.%u)\n", NIPQUAD(args->entries[i].eg_sipv4), NIPQUAD(args->entries[i].eg_dipv4));
-	    }
-	} else{
-	    printf("unknown packet type! (pkt_type=%d) \n", args->entries[i].pkt_type);
-	}
+    /* 3. 使用您驗證過的「修正版」顯示邏輯 */
+    for(i = 0; i < args->num_of_entries; i++) {
+        /* --- 提取資料到局部變數 (這是修正 IP 顯示的關鍵) --- */
+        unsigned int i_sip = args->entries[i].ing_sipv4;
+        unsigned int i_dip = args->entries[i].ing_dipv4;
+        unsigned int e_sip = args->entries[i].eg_sipv4;
+        unsigned int e_dip = args->entries[i].eg_dipv4;
+
+        /* IPv6 輔助變數 */
+        unsigned int is6[4], id6[4], es6[4], ed6[4];
+        is6[0] = args->entries[i].ing_sipv6_0; is6[1] = args->entries[i].ing_sipv6_1;
+        is6[2] = args->entries[i].ing_sipv6_2; is6[3] = args->entries[i].ing_sipv6_3;
+        id6[0] = args->entries[i].ing_dipv6_0; id6[1] = args->entries[i].ing_dipv6_1;
+        id6[2] = args->entries[i].ing_dipv6_2; id6[3] = args->entries[i].ing_dipv6_3;
+        es6[0] = args->entries[i].eg_sipv6_0; es6[1] = args->entries[i].eg_sipv6_1;
+        es6[2] = args->entries[i].eg_sipv6_2; es6[3] = args->entries[i].eg_sipv6_3;
+        ed6[0] = args->entries[i].eg_dipv6_0; ed6[1] = args->entries[i].eg_dipv6_1;
+        ed6[2] = args->entries[i].eg_dipv6_2; ed6[3] = args->entries[i].eg_dipv6_3;
+
+        /* --- 格式化輸出 (使用修正後的變數) --- */
+        if(args->entries[i].pkt_type == 0) { // IPV4_NAPT
+            printf("IPv4_NAPT=%d : %u.%u.%u.%u:%d->%u.%u.%u.%u:%d => %u.%u.%u.%u:%d->%u.%u.%u.%u:%d\n",
+                args->entries[i].hash_index,
+                NIPQUAD(i_sip), args->entries[i].ing_sp,
+                NIPQUAD(i_dip), args->entries[i].ing_dp,
+                NIPQUAD(e_sip), args->entries[i].eg_sp,
+                NIPQUAD(e_dip), args->entries[i].eg_dp);
+        }
+        else if(args->entries[i].pkt_type == 1) { // IPV4_NAT
+            printf("IPv4_NAT=%d : %u.%u.%u.%u->%u.%u.%u.%u => %u.%u.%u.%u->%u.%u.%u.%u\n",
+                args->entries[i].hash_index,
+                NIPQUAD(i_sip),
+                NIPQUAD(i_dip),
+                NIPQUAD(e_sip),
+                NIPQUAD(e_dip));
+        }
+        else if(args->entries[i].pkt_type == 2) { // IPV6_ROUTING
+            printf("IPv6_1T= %d /DIP: %x:%x:%x:%x:%x:%x:%x:%x\n",
+                args->entries[i].hash_index,
+                NIPHALF(id6[0]), NIPHALF(id6[1]), NIPHALF(id6[2]), NIPHALF(id6[3]));
+        }
+        else if(args->entries[i].pkt_type == 3) { // IPV4_DSLITE
+            printf("DS-Lite= %d : %u.%u.%u.%u:%d->%u.%u.%u.%u:%d (%x:%x:%x:%x:%x:%x:%x:%x -> %x:%x:%x:%x:%x:%x:%x:%x) \n",
+                args->entries[i].hash_index,
+                NIPQUAD(i_sip), args->entries[i].ing_sp,
+                NIPQUAD(i_dip), args->entries[i].ing_dp,
+                NIPHALF(es6[0]), NIPHALF(es6[1]), NIPHALF(es6[2]), NIPHALF(es6[3]),
+                NIPHALF(ed6[0]), NIPHALF(ed6[1]), NIPHALF(ed6[2]), NIPHALF(ed6[3]));
+        }
+        else if(args->entries[i].pkt_type == 4) { // IPV6_3T_ROUTE
+            printf("IPv6_3T= %d SIP: %x:%x:%x:%x:%x:%x:%x:%x DIP: %x:%x:%x:%x:%x:%x:%x:%x\n",
+                args->entries[i].hash_index,
+                NIPHALF(is6[0]), NIPHALF(is6[1]), NIPHALF(is6[2]), NIPHALF(is6[3]),
+                NIPHALF(id6[0]), NIPHALF(id6[1]), NIPHALF(id6[2]), NIPHALF(id6[3]));
+        }
+        else if(args->entries[i].pkt_type == 5) { // IPV6_5T_ROUTE
+            if(args->entries[i].ipv6_flowlabel == 1) {
+                printf("IPv6_5T= %d SIP: %x:%x:%x:%x:%x:%x:%x:%x DIP: %x:%x:%x:%x:%x:%x:%x:%x (Flow Label=%x)\n",
+                    args->entries[i].hash_index,
+                    NIPHALF(is6[0]), NIPHALF(is6[1]), NIPHALF(is6[2]), NIPHALF(is6[3]),
+                    NIPHALF(id6[0]), NIPHALF(id6[1]), NIPHALF(id6[2]), NIPHALF(id6[3]),
+                    ((args->entries[i].ing_sp << 16) | (args->entries[i].ing_dp)) & 0xFFFFF);
+            } else {
+                printf("IPv6_5T= %d SIP: %x:%x:%x:%x:%x:%x:%x:%x (SP:%d) DIP: %x:%x:%x:%x:%x:%x:%x:%x (DP=%d)\n",
+                    args->entries[i].hash_index,
+                    NIPHALF(is6[0]), NIPHALF(is6[1]), NIPHALF(is6[2]), NIPHALF(is6[3]), args->entries[i].ing_sp,
+                    NIPHALF(id6[0]), NIPHALF(id6[1]), NIPHALF(id6[2]), NIPHALF(id6[3]), args->entries[i].ing_dp);
+            }
+        }
+        else if(args->entries[i].pkt_type == 7) { // IPV6_6RD
+            if(args->entries[i].ipv6_flowlabel == 1) {
+                printf("6RD= %d %x:%x:%x:%x:%x:%x:%x:%x->%x:%x:%x:%x:%x:%x:%x:%x [Flow Label=%x]\n",
+                    args->entries[i].hash_index,
+                    NIPHALF(is6[0]), NIPHALF(is6[1]), NIPHALF(is6[2]), NIPHALF(is6[3]),
+                    NIPHALF(id6[0]), NIPHALF(id6[1]), NIPHALF(id6[2]), NIPHALF(id6[3]),
+                    ((args->entries[i].ing_sp << 16) | (args->entries[i].ing_dp)) & 0xFFFFF);
+                printf("(%u.%u.%u.%u->%u.%u.%u.%u)\n", NIPQUAD(e_sip), NIPQUAD(e_dip));
+            } else {
+                printf("6RD= %d /SIP: %x:%x:%x:%x:%x:%x:%x:%x [SP:%d] /DIP: %x:%x:%x:%x:%x:%x:%x:%x [DP=%d] ",
+                    args->entries[i].hash_index,
+                    NIPHALF(is6[0]), NIPHALF(is6[1]), NIPHALF(is6[2]), NIPHALF(is6[3]), args->entries[i].ing_sp,
+                    NIPHALF(id6[0]), NIPHALF(id6[1]), NIPHALF(id6[2]), NIPHALF(id6[3]), args->entries[i].ing_dp); 
+                printf("(%u.%u.%u.%u->%u.%u.%u.%u)\n", NIPQUAD(e_sip), NIPQUAD(e_dip));
+            }
+        }
+        else {
+            printf("unknown packet type! (pkt_type=%d) \n", args->entries[i].pkt_type);
+        }
     }
 
     result = args->result;
